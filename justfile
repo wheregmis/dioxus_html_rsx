9c8436afcfa3933fd479fc59ffdc183105b91c44
Sabin Regmi <get2sabin@gmail.com>
2024-12-04 21:26:19 -0500

    Github Pages Host

--- justfile
+++ justfile
@@ -2,7 +2,7 @@ check:
     cargo check
 
 run:
-    dx serve --platform fullstack
+    dx serve --platform fullstack --port 3000
 
 css:
     npx tailwindcss -i ./input.css -o ./public/tailwind.css --watch
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,19 @@
-//! Run with:
-//!
-//! ```sh
-//! dx serve --platform fullstack
-//! ```
-
 use dioxus::prelude::*;
-#[cfg(feature = "server")]
-use std::fs::File;
-#[cfg(feature = "server")]
-use std::io::Write;
-#[cfg(feature = "server")]
-use std::process::Command;
+use dioxus_rsx_rosetta::Dom;
 
-const STYLE: &str = manganis::mg!("public/tailwind.css");
+const MAIN_CSS: Asset = asset!("/public/tailwind.css");
 
 fn main() {
-    LaunchBuilder::fullstack()
-        .with_cfg(server_only!(ServeConfig::builder().incremental(
-            IncrementalRendererConfig::default()
-                .invalidate_after(std::time::Duration::from_secs(120)),
-        )))
-        .launch(app);
-}
-
-fn app() -> Element {
-    rsx! {
-        Router::<Route> {}
-    }
-}
-
-#[derive(Clone, Routable, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
-enum Route {
-    #[route("/")]
-    Home {},
+    dioxus::launch(app);
 }
 
 #[component]
-fn Home() -> Element {
-    let mut html_input = use_signal(|| String::new());
+fn app() -> Element {
+    let mut html_input = use_signal(String::new);
     let mut rsx_output = use_signal(|| "Your Generated RSX".to_string());
 
     rsx! {
-        head::Link { rel: "stylesheet", href: STYLE }
+        link { rel: "stylesheet", href: MAIN_CSS }
         div { style: "flex flex-col items-center justify-center h-screen",
             div { class: "flex flex-row items-center justify-center w-full",
                 div { class: "flex flex-col items-center justify-center w-full h-screen",
@@ -53,18 +25,12 @@ fn Home() -> Element {
                     }
                     button {
                         class: "btn m-5",
-                        onclick: move |event| {
+                        onclick: move |_| {
                             spawn(async move {
-                                println!("Clicked! {event:?}");
-                                let rsx_response = post_server_data(html_input()).await;
-                                match rsx_response {
-                                    Ok(rsx_out) => {
-                                        rsx_output.set(rsx_out);
-                                    }
-                                    Err(_) => {
-                                        rsx_output.set("Error!".to_string());
-                                    }
-                                }
+                                let dom = Dom::parse(&html_input()).unwrap();
+                                let rsx_response_callbody = dioxus_rsx_rosetta::rsx_from_html(&dom);
+                                rsx_output
+                                    .set(dioxus_autofmt::write_block_out(&rsx_response_callbody).unwrap());
                             });
                         },
                         "Convert HTML to RSX"
@@ -80,31 +46,4 @@ fn Home() -> Element {
             }
         }
     }
-}
-
-#[server(PostServerData)]
-async fn post_server_data(data: String) -> Result<String, ServerFnError> {
-    println!("Server received: {}", data);
-
-    let output = Command::new("dx")
-        .arg("translate")
-        .arg("--raw")
-        .arg(data) // Or pass the HTML string directly if supported
-        .output()?;
-
-    // Step 4: Process the output
-    if output.status.success() {
-        let rsx_output = String::from_utf8_lossy(&output.stdout);
-        println!("RSX Output: {}", rsx_output);
-        Ok(rsx_output.to_string())
-    } else {
-        let error_message = String::from_utf8_lossy(&output.stderr);
-        eprintln!("Error: {}", error_message);
-        Err(ServerFnError::new("Error converting HTML to RSX"))
-    }
-}
-
-#[server(GetServerData)]
-async fn get_server_data() -> Result<String, ServerFnError> {
-    Ok("Hello from the server!".to_string())
 }
